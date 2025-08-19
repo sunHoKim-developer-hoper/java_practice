cf7bbdfdc2c75caba1d7ef7ce91313750d4da115
KIM <gimseonho4@gmail.com>
2025-08-19 17:06:02 +0900

    volatile 키워드 학습

---
+++ src/thread/volatile1/VolatileCountMain.java
@@ -0,0 +1,32 @@
+package thread.volatile1;
+
+import static thread.util.MyLogger.log;
+import static thread.util.ThreadUtils.sleep;
+
+public class VolatileCountMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task,"myTask");
+        t.start();
+        sleep(1000);
+        task.flag = false;
+        log("flag : " + task.flag + ", count : "+ task.count + "종료");
+    }
+
+    static class MyTask implements Runnable {
+        volatile boolean flag = true;
+        volatile long count = 0;
+
+        @Override
+        public void run() {
+            while(flag){
+                count ++;
+                //1억번에 한 번씩 출력
+                if(count % 100_000_000 == 0){
+                    log("flag : " + flag + ", count = "+ count + "in while()");
+                }
+            }
+            log("flag : " + flag + ", count : "+ count + "종료");
+        }
+    }
+}
